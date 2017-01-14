f568074a024013685d4e816e1f1bd5e2da8ba0e9
Talonj123 <jtalbert@iastate.edu>
2017-01-14 14:39:22 -0600

    I think I cleaded somethings, we'll find out.

--- FW2017/Base Station Communication/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"G:/Data/MAVRIC/MAVRIC_Firmware/FW2017/Shared/Debug/Shared.lib" -llibc.a
-
